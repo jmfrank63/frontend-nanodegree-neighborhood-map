e1a2c0cff62e59587c49ba8b4febb073819e221c
Johannes Frank <jmfrank63@gmail.com>
2015-05-15 09:51:27 +0000

    Technically working

--- js/markerViewModel.js
+++ js/markerViewModel.js
@@ -5,26 +5,38 @@
 
 function markerViewModel () {
     var self = this;
-    self.venues = [];
+    self.pool = [];
+    self.title = ko.observable('Neighborhood Map');
     self.keep = ko.observable(false);
     self.radius = ko.observable(200);
     self.limit = ko.observable(50);
-    self.markers = ko.observableArray()
+    self.markers = ko.observableArray();
     self.search = ko.observable('');
     self.filter = ko.observable('');
-    
+
     // function to be called on submit filter
     self.submitFilter = function (formElement) {
-      console.log(self.filter());  
+        var markers = [];
+        var filterString = self.filter().split('|');
+        self.markers([]);
+        $.each(filterString, function (i, filter) {
+                $.extend(markers, $.grep(self.pool, function(elem) {
+                    console.log(filter.toLowerCase());
+                    return (elem.name.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+                }, false));
+        });
+        ko.utils.arrayPushAll(self.markers(), markers);
+        self.markers.valueHasMutated();
+        self.drawMarkers(self.markers());
     };
 
     // function to be called on submit search
     self.submitSearch = function (formElement) {
-        var queryString = self.submit().split('|');
+        var queryString = self.search().split('|');
         console.log(self.keep());
         if(!self.keep()) {
             self.markers([]);
-            self.venues = [];
+            self.pool = [];
             map.removeMarkers();
         }
         // call the foursquare api with supplied queries
@@ -34,15 +46,20 @@ function markerViewModel () {
     };
     // filter out relevant information of the venues
     self.getMarkers = function (result) {
+        if(result.meta.code !== 200) {
+            console.log(self.title());
+            return;
+        }
+        self.title('Neighborhood Map');
         venues = result.response.venues;
         var marker, res;
         var markers = [];
         $.each(venues, function (i, venue) {
-            marker = filterMarker(venue);
+            marker = refineMarker(venue);
             res = $.grep(self.markers(), function (elem) {
                 return elem.id === marker.id});
             if (res.length == 0) {
-                self.venues.push(marker);
+                self.pool.push(marker);
                 markers.push(marker);
             }
         });
@@ -51,16 +68,25 @@ function markerViewModel () {
         ko.utils.arrayPushAll(self.markers(), markers);
         self.markers.valueHasMutated();
 
-        $.each(self.markers(), function (i, value) {
+        self.drawMarkers(self.markers());
+    };
+
+    self.drawMarkers = function (markers) {
+        map.removeMarkers();
+        $.each(markers, function (i, value) {
             map.addMarker(value.mapMarker);
         });
-    }
-    
+    };
+
     self.openInfoWindow = function (marker) {
         map.hideInfoWindows();
         (marker.mapMarker.infoWindow).open(marker.mapMarker.map, marker.mapMarker);
     };
     
+    $('.dropdown-menu input, .dropdown-menu label').click(function(e) {
+        e.stopPropagation();
+    });
+
     $.each('Pérola|Carris|bar'.split('|'), function (i, value) {
         foursquare( {query: value, radius: 200, limit: 50 }, self.getMarkers);
     });
@@ -69,8 +95,9 @@ function markerViewModel () {
 
 
 // filter out the relevant info from a venue
-function filterMarker(venue) {
+function refineMarker(venue) {
     var marker = {};
+    marker.visible = true;
     marker.id = venue.id;
     marker.name = venue.name;
     if (venue.categories.length > 0) {
