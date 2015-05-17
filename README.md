4bad54ba9f0452e0c916c84572b387c0f314c177
Johannes Frank <jmfrank63@gmail.com>
2015-05-17 01:17:24 +0000

    Final version ready for upload

--- README.md
+++ README.md
@@ -1,27 +1,98 @@
 # frontend-nanodegree-neighborhood-map
 
-Review our course JavaScript Design Patterns.
+## Install the site
 
-Download the Knockout framework.
+### Downloading
 
-Write code required to add a full-screen map to your page using the Google Maps API.
+Download the zip file from: https://github.com/jmfrank63/frontend-nanodegree-neighborhood-map/archive/master.zip
 
-Write code required to add map markers identifying a number of locations your are interested in within this neighborhood.
+Extract with uzip and change into the created directory.
 
-Implement the search bar functionality to search and filter your map markers. There should be a filtering function on markers that already show up. Simply providing a search function through a third-party API is not enough.
+With your favoirite brower open index.html
 
-Implement a list view of the identified locations.
+### Git
 
-Add additional functionality using third-party APIs when a map marker, search result, or list view entry is clicked (ex. Yelp reviews, Wikipedia, Flickr images, etc). If you need a refresher on making AJAX requests to third-party servers, check out our Intro to AJAX course.
+Install git
 
-Helpful Resources
+git clone https://github.com/jmfrank63/frontend-nanodegree-neighborhood-map.git
 
-None of these are required, but they may be helpful.
+Change into the created directory.
 
-Foursquare API
+With your favorite browser open index.html
 
-MediaWikiAPI for Wikipedia
+### On the internet:
 
-Google Maps Street View Service
+Goto Url: https://frontend-nanodegree-neighborhood-map-jmfrank63.c9.io/index.html
 
-Google Maps
+## Use the site
+
+The page starts at the authors current home location in Lisbon, Portugal.
+
+A few interesting places are shown. On the left top site the writing "Neighborhood Map"
+appears. If anything goes wrong this text changes to the error message.
+
+To search either just click on the magnifying class or enter a search term into
+the search box. Only letters are supported but any substring matches. If you enter just
+a single letter you get results for all terms containing this letter.
+
+Multiple search terms are supported, to separate them use the pipe | symbol. The result
+will be the union of all search terms. Double finds are singled out.
+
+Under the preferences you can tap the Keep results checkbox. If checked subsequent searches
+are added to the previous ones instead of overwriting them.
+
+After searching you can filter your results. Filter terms work similar to search terms
+with the exception that the filter will only work on the previously found searches.
+Again multiple filter terms are supported by separating them with the pipe symbol.
+
+In addition negation is supported as well. To negate a filter term prefix it with an
+exclamation mark.
+
+Under the preferences you have additional controls: Limit limits the number of searches
+to a maximum number. Internally a maximum of 50 can be handled although no checking is done
+in the value field.
+
+In the radius field you enter the search radius in meters. A maxiumum of 10000 is supported,
+although again the values are not checked.
+
+To move the center of the map to your current location press the button behind locate me!
+To move the center to latitude longitude coordinates enter them in the fields below locate
+me!
+Finally to move to an address enter it in the address box.
+
+Next to the preferences symbol you'll find a list of all current active markers. If you click
+on a list item the infobox of the corresponding marker opens. You can open the infobox by
+clicking on the marker as well.
+
+Scrolling the map around sets the center to a new location. You can take a look at its coordinates
+by looking at the values in the corresponding preferences fields.
+
+The searches are all done with calls to the foursquare api.
+
+--------------------------------------------------------------
+
+Comments to the instructors
+
+Of course one could think of a lot of additional functionallity to be implemented.
+Marker at the center, reverse geocoding, preference for zoom level, listing
+the catagories of the searches and being able to filter for them...
+I only had roughly two weeks and a 50 hour job with late shifts, so I apologize for
+rough edges. The preferences menu should be styled better, and other minor issues.
+
+I did some pagespeed optimization, but did not get past the 78/100 for mobile.
+Reducing bootstrap with uncss or grunt is a little work, and time is running out.
+
+Html and css files are w3c compatiple and have been checked.
+
+map.js is jslint ok.
+
+Please find the unoptimized version under index.large.html and the optimized under
+index.html.
+The all.js is a simple
+$ cat jquery-2.1.3.js bootstrap.js knockout-3.3.0.js gmaps.js ../map.js >all.js
+along with a
+yui-compressor -o all.min.js all.js
+
+Nevertheless I am quite pleased I made it in time. I hope I have met all specifcations,
+but I am ready to do any adjustments required.
+
--- js/lib/all.js
+++ js/lib/all.js
@@ -13839,203 +13839,59 @@ if (!Array.prototype.indexOf) {
   
 return GMaps;
 }));
+/*global $, GMaps, ko */
+/*jshint unused: true, node: true */
+/*jslint unparam: true, node: true */
+
 /* Map view model
  * contains knockout observables
- * and the map
+ * the map
  */
-// the map should be accessable by other modules too
-//var map;
-
+'use strict';
+// the map should be globally accessable
 var map;
 
-$(function () {
-    'use strict';
-    
-    var mapCenter = { lat: 38.706495,  lng: -9.156769 };
-    
+$((function () {
+    // the initial coordinates
+    var mapCenter = {lat: 38.706495, lng: -9.156769};
     // define a new map
     map = new GMaps({
         div: '#map-canvas',
         center: mapCenter,
         zoom: 17
     });
-    
-} ());
+}()));
 
 var foursquare = function (query, callback) {
     /* Foursquare api module
      * comunicates with the foursquare api
      * returns json data
      */
-    
+
     // Defaults for the api
-    var FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/search';
-    var CLIENT_ID = '0NF4PAQ2Q0HIF2XYTB5ICX135ZMSLYJXSSG0AMCFTMDA0M1G';
-    var CLIENT_SECRET = 'OJBZPIO4OQGKH3TP3BQUJJXCODPIVZAZEVMKLAWG1B1J2J5Z';
-    var VERSION = '20150511';
-    var HOME = '38.706495,-9.156769';
-    
-    // JSON object holding foursquare data
-    var defaultData = {
-        client_id : CLIENT_ID,
-        client_secret : CLIENT_SECRET,
-        v : VERSION ,
-        ll : HOME
-    };
-    
-    var data = $.extend({}, defaultData, query);
-        
+    var FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/search',
+        CLIENT_ID = '0NF4PAQ2Q0HIF2XYTB5ICX135ZMSLYJXSSG0AMCFTMDA0M1G',
+        CLIENT_SECRET = 'OJBZPIO4OQGKH3TP3BQUJJXCODPIVZAZEVMKLAWG1B1J2J5Z',
+        VERSION = '20150511',
+        HOME = '38.706495,-9.156769',
+        // JSON object holding foursquare data
+        defaultData = {
+            client_id : CLIENT_ID,
+            client_secret : CLIENT_SECRET,
+            v : VERSION,
+            ll : HOME
+        },
+
+        // merge default data with given options overwriting the defaults
+        data = $.extend({}, defaultData, query);
+
+    // the actual ajax call
     $.getJSON(FOURSQUARE_URL, data, callback);
 };
 
-function markerViewModel () {
-    var self = this;
-    self.pool = [];
-    self.lat = ko.observable(map.getCenter().A);
-    self.lng = ko.observable(map.getCenter().F);
-    self.address = ko.observable('');
-    self.title = ko.observable('Neighborhood Map');
-    self.keep = ko.observable(false);
-    self.radius = ko.observable(200);
-    self.limit = ko.observable(50);
-    self.markers = ko.observableArray();
-    self.search = ko.observable('');
-    self.filter = ko.observable('');
-
-    self.lng.subscribe(function (newValue) {
-        map.setCenter(self.lat(), self.lng());   
-    });
-    
-    self.lat.subscribe(function (newValue) {
-        map.setCenter(self.lat(), self.lng());   
-    });
-    
-    
-    // function to be called on submit filter
-    self.submitFilter = function (formElement) {
-        var markers = [];
-        var filterString = self.filter().split('|');
-        self.markers([]);
-        $.each(filterString, function (i, filter) {
-                var invert = false;
-                if(filter[0] === '!') {
-                    filter = filter.slice(1,filter.length - 1);
-                    invert = true;
-                }
-                $.extend(markers, $.grep(self.pool, function(elem) {
-                    console.log(filter.toLowerCase());
-                    return (elem.name.toLowerCase().indexOf(filter.toLowerCase()) > -1);
-                }, invert));
-        });
-        ko.utils.arrayPushAll(self.markers(), markers);
-        self.markers.valueHasMutated();
-        self.drawMarkers(self.markers());
-    };
-
-    // function to be called on submit search
-    self.submitSearch = function (formElement) {
-        var center = map.getCenter();
-        self.lat(center.A);
-        self.lng(center.F);
-        map.hideInfoWindows();
-        var queryString = self.search().split('|');
-        if(!self.keep()) {
-            self.markers([]);
-            self.pool = [];
-            map.removeMarkers();
-        }
-        // call the foursquare api with supplied queries
-        $.each(queryString, function (i, value) {
-            foursquare( {ll: self.lat() + ',' + self.lng(), query: value, radius: self.radius(), limit: self.limit() }, self.getMarkers);
-        });
-    };
-    // filter out relevant information of the venues
-    self.getMarkers = function (result) {
-        if(result.meta.code !== 200) {
-            console.log(self.title());
-            return;
-        }
-        self.title('Neighborhood Map');
-        venues = result.response.venues;
-        var marker, res;
-        var markers = [];
-        $.each(venues, function (i, venue) {
-            marker = refineMarker(venue);
-            res = $.grep(self.markers(), function (elem) {
-                return elem.id === marker.id});
-            if (res.length == 0) {
-                self.pool.push(marker);
-                markers.push(marker);
-            }
-        });
-        // inform the observable array of new markers
-        // do this in one step instead for each element
-        ko.utils.arrayPushAll(self.markers(), markers);
-        self.markers.valueHasMutated();
-
-        self.drawMarkers(self.markers());
-    };
-
-    self.drawMarkers = function (markers) {
-        map.removeMarkers();
-        $.each(markers, function (i, value) {
-            map.addMarker(value.mapMarker);
-        });
-    };
-
-    self.openInfoWindow = function (marker) {
-        map.hideInfoWindows();
-        (marker.mapMarker.infoWindow).open(marker.mapMarker.map, marker.mapMarker);
-    };
-    
-    self.geoLocate = function() {
-      GMaps.geolocate({
-          success: function(position) {
-              self.title('Neighborhood Map');
-              self.lat(position.coords.latitude);
-              self.lng(position.coords.longitude);
-              map.setCenter(position.coords.latitude, position.coords.longitude);
-          },
-          error: function(error) {
-            self.title('Geolocation failed: '+error.message);
-          },
-          not_supported: function() {
-            self.title("Your browser does not support geolocation");
-          },
-          always: function() {
-          }
-        });  
-    };
-    
-    self.geoCode = function () {
-        GMaps.geocode({
-            address: self.address(),
-                callback: function(results, status) {
-                    if (status == 'OK') {
-                        self.title('Neighborhood Map');
-                        var latlng = results[0].geometry.location;
-                        map.setCenter(latlng.lat(), latlng.lng());
-                        self.lat(latlng.lat());
-                        self.lng(latlng.lng());
-                    } else {
-                        self.title('Address not found');
-                    }
-                }
-            });
-    };
-    
-    $('.dropdown-menu input, .dropdown-menu label').click(function(e) {
-        e.stopPropagation();
-    });
-
-    $.each('Pérola|Carris|bar'.split('|'), function (i, value) {
-        foursquare( {query: value, radius: 200, limit: 50 }, self.getMarkers);
-    });
-}
-
-
-
 // filter out the relevant info from a venue
+// since we do not use the mapping plugin we
+// have to do this by our own
 function refineMarker(venue) {
     var marker = {};
     marker.visible = true;
@@ -14043,8 +13899,7 @@ function refineMarker(venue) {
     marker.name = venue.name;
     if (venue.categories.length > 0) {
         marker.category = venue.categories[0].name;
-    }
-    else {
+    } else {
         marker.category = '';
     }
     marker.lat = venue.location.lat;
@@ -14062,10 +13917,175 @@ function refineMarker(venue) {
     return marker;
 }
 
-var mvm = new markerViewModel();
+// the knockout viewModel
+function MarkerViewModel() {
+    var self = this;
+    self.pool = [];
+    self.lat = ko.observable(map.getCenter().A);
+    self.lng = ko.observable(map.getCenter().F);
+    self.address = ko.observable('');
+    self.title = ko.observable('Neighborhood Map');
+    self.keep = ko.observable(false);
+    self.radius = ko.observable(200);
+    self.limit = ko.observable(50);
+    self.markers = ko.observableArray();
+    self.search = ko.observable('');
+    self.filter = ko.observable('');
+
+    // if the center changes update the observables
+    self.lng.subscribe(function () {
+        self.title('Neighborhood Map');
+        map.setCenter(self.lat(), self.lng());
+    });
+    self.lat.subscribe(function () {
+        self.title('Neighborhood Map');
+        map.setCenter(self.lat(), self.lng());
+    });
+
+    // function to be called on submit filter
+    self.submitFilter = function (formElement) {
+        // clear all markers and fill them again with filtered markers
+        var markers = [],
+            // split the filter string into an array and loop over it
+            filterString = self.filter().split('|');
+        self.markers([]);
+        $.each(filterString, function (i, filter) {
+            var invert = false;
+            // check for invertion exclamation marks
+            if (filter[0] === '!') {
+                filter = filter.slice(1, filter.length - 1);
+                invert = true;
+            }
+            // find all matching filter terms
+            $.extend(markers, $.grep(self.pool, function (elem) {
+                return (elem.name.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+            }, invert));
+        });
+        // now do the update to the observable array
+        // do this in one call and then notify the change
+        ko.utils.arrayPushAll(self.markers(), markers);
+        self.markers.valueHasMutated();
+        // finally update the markes
+        self.drawMarkers(self.markers());
+    };
+
+    // function to be called on submit search
+    self.submitSearch = function (formElement) {
+        // get the center so the search is located in
+        // the visible area
+        var center = map.getCenter(),
+        // split the query string into an array of search terms
+            queryString = self.search().split('|');
+        self.lat(center.A);
+        self.lng(center.F);
+        // hide infoboxes prior to searching
+        map.hideInfoWindows();
+        if (!self.keep()) {
+            self.markers([]);
+            self.pool = [];
+            map.removeMarkers();
+        }
+        // call the foursquare api with supplied queries
+        $.each(queryString, function (i, value) {
+            foursquare({ll: self.lat() + ',' + self.lng(), query: value, radius: self.radius(), limit: self.limit()}, self.getMarkers);
+        });
+    };
+    // filter out relevant information of the venues
+    self.getMarkers = function (result) {
+        // check if api call was successful
+        // if not show error message
+        if (result.meta.code !== 200) {
+            self.title('Results are not available!');
+            return;
+        }
+        // show title if everything is ok
+        self.title('Neighborhood Map');
+        var venues = result.response.venues,
+            marker,
+            res,
+            markers = [];
+        // loop over all places and add them to the observable array
+        $.each(venues, function (i, venue) {
+            marker = refineMarker(venue);
+            res = $.grep(self.markers(), function (elem) {
+                return (elem.id === marker.id);
+            });
+            if (res.length === 0) {
+                self.pool.push(marker);
+                markers.push(marker);
+            }
+        });
+        // inform the observable array of new markers
+        // do this in one step instead for each element
+        ko.utils.arrayPushAll(self.markers(), markers);
+        self.markers.valueHasMutated();
+        // finally draw the markers
+        self.drawMarkers(self.markers());
+    };
+
+    // put the markers in the array into the map
+    self.drawMarkers = function (markers) {
+        map.removeMarkers();
+        $.each(markers, function (i, value) {
+            map.addMarker(value.mapMarker);
+        });
+    };
+
+    // open the infobox of a marker
+    self.openInfoWindow = function (marker) {
+        map.hideInfoWindows();
+        (marker.mapMarker.infoWindow).open(marker.mapMarker.map, marker.mapMarker);
+    };
+
+    // locate the position of the user
+    self.geoLocate = function () {
+        GMaps.geolocate({
+            success: function (position) {
+                self.title('Neighborhood Map');
+                self.lat(position.coords.latitude);
+                self.lng(position.coords.longitude);
+                map.setCenter(position.coords.latitude, position.coords.longitude);
+            },
+            error: function (error) {
+                self.title('Geolocation failed: ' + error.message);
+            },
+            not_supported: function () {
+                self.title("Your browser does not support geolocation");
+            }
+        });
+    };
+
+    self.geoCode = function () {
+        GMaps.geocode({
+            address: self.address(),
+            callback: function (results, status) {
+                if (status === 'OK') {
+                    self.title('Neighborhood Map');
+                    var latlng = results[0].geometry.location;
+                    map.setCenter(latlng.lat(), latlng.lng());
+                    self.lat(latlng.lat());
+                    self.lng(latlng.lng());
+                } else {
+                    self.title('Address not found');
+                }
+            }
+        });
+    };
+
+    // prevent the preferences dropdown menu from closing when clicked
+    $('.dropdown-menu input, .dropdown-menu label').click(function (event) {
+        event.stopPropagation();
+    });
+
+    // put some starting values on the map
+    $.each(['Pérola', 'Carris', 'bar'], function (i, value) {
+        foursquare({query: value, radius: 200, limit: 50 }, self.getMarkers);
+    });
+}
+
+
+// create the viewModel
+var mvm = new MarkerViewModel();
+
+// apply the knockout mapping
 ko.applyBindings(mvm);
-
-
-// setTimeout(function() {
-//     console.log(mvm.markers());
-// }, 10000);
